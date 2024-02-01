21f491a9d65f3de9c2bf6a6036621d46a67399f2
leonyllukejayme <leonyljayme@gmail.com>
2024-02-01 13:14:57 +0800

    number to Number

--- Backend/models/userModel.js
+++ Backend/models/userModel.js
@@ -7,7 +7,7 @@ const userSchema = new Schema({
 	email: { type: String, required: true },
 	password: { type: String, required: true },
 	avatar: { type: String },
-	posts: { type: number, default: 0 },
+	posts: { type: Number, default: 0 },
 });
 
 module.exports = mongoose.model('User',userSchema)
