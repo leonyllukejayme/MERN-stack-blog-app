98f28faf1dd37131f44782fce9ea0656edcbbeb3
leonyllukejayme <leonyljayme@gmail.com>
2024-02-26 21:52:14 +0800

    Frontend - Login axios post

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,47 +1,53 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import Layout from './components/Layout'
-import ErrorPage from './pages/ErrorPage'
-import Home from './pages/Home'
-import PostDetail from './pages/PostDetail'
-import Register from './pages/Register'
-import Login from './pages/Login'
-import UserProfile from './pages/UserProfile'
-import Authors from './pages/Authors'
-import CreatePost from './pages/CreatePost'
-import CategoryPosts from './pages/CategoryPosts'
-import AuthorPosts from './pages/AuthorPosts'
-import Dashboard from './pages/Dashboard'
-import EditPost from './pages/EditPost'
-import Logout from './pages/Logout'
-import DeletePost from './pages/DeletePost'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Layout from './components/Layout';
+import UserProvider from './context/userContext';
+import './index.css';
+import AuthorPosts from './pages/AuthorPosts';
+import Authors from './pages/Authors';
+import CategoryPosts from './pages/CategoryPosts';
+import CreatePost from './pages/CreatePost';
+import Dashboard from './pages/Dashboard';
+import DeletePost from './pages/DeletePost';
+import EditPost from './pages/EditPost';
+import ErrorPage from './pages/ErrorPage';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Logout from './pages/Logout';
+import PostDetail from './pages/PostDetail';
+import Register from './pages/Register';
+import UserProfile from './pages/UserProfile';
 
-const router = createBrowserRouter([{
-  path:'/',
-  element:<Layout />,
-  errorElement:<ErrorPage />,
-  children: [
-    {index:true,element:<Home />},
-    {path:'posts/:id',element:<PostDetail />},
-    {path:'register',element:<Register />},
-    {path:'login',element:<Login />},
-    {path:'profile/:id',element:<UserProfile />},
-    {path:'authors',element:<Authors />},
-    {path:'create',element:<CreatePost />},
-    {path:'posts/categories/:category',element:<CategoryPosts />},
-    {path:'posts/users/:id',element:<AuthorPosts />},
-    {path:'myposts/:id',element:<Dashboard />},
-    {path:'posts/edit/:id',element:<EditPost />},
-    {path:'posts/delete/:id',element:<DeletePost />},
-    {path:'logout',element:<Logout />},
-  ]
-}])
-
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: (
+			<UserProvider>
+				<Layout />
+			</UserProvider>
+		),
+		errorElement: <ErrorPage />,
+		children: [
+			{ index: true, element: <Home /> },
+			{ path: 'posts/:id', element: <PostDetail /> },
+			{ path: 'register', element: <Register /> },
+			{ path: 'login', element: <Login /> },
+			{ path: 'profile/:id', element: <UserProfile /> },
+			{ path: 'authors', element: <Authors /> },
+			{ path: 'create', element: <CreatePost /> },
+			{ path: 'posts/categories/:category', element: <CategoryPosts /> },
+			{ path: 'posts/users/:id', element: <AuthorPosts /> },
+			{ path: 'myposts/:id', element: <Dashboard /> },
+			{ path: 'posts/edit/:id', element: <EditPost /> },
+			{ path: 'posts/delete/:id', element: <DeletePost /> },
+			{ path: 'logout', element: <Logout /> },
+		],
+	},
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
+);
--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -1,32 +1,71 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import axios from 'axios';
+import { useContext, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { UserContext } from '../context/userContext';
 
 const Login = () => {
-  const [userData, setUserData] = useState({
-    email:'',
-    password:'',
-  })
+	const [userData, setUserData] = useState({
+		email: '',
+		password: '',
+	});
 
-  const changeInputHandler = (e) => {
-    setUserData(prevState => {
-      return {...prevState, [e.target.name]: e.target.value}
-    })
-  }
+	const [error, setError] = useState('');
+	const navigate = useNavigate();
 
-  return (
-    <section className="login">
-      <div className="container">
-        <h2>Sign In</h2>
-        <form className="form login__form">
-          <p className="form__error-message">This is an Error message</p>
-          <input type="email" placeholder='Email' name='email' value={userData.email} onChange={changeInputHandler} />
-          <input type="password" placeholder='Password' name='password' value={userData.password} onChange={changeInputHandler} />
-          <button type="submit" className='btn primary'>Login</button>
-        </form>
-        <small>Don't have an account yet? <Link to={'/register'}>Sign up</Link></small>
-      </div>
-    </section>
-  )
-}
+	const { setCurrentUser } = useContext(UserContext);
 
-export default Login
+	const changeInputHandler = (e) => {
+		setUserData((prevState) => {
+			return { ...prevState, [e.target.name]: e.target.value };
+		});
+	};
+
+	const loginUser = async (e) => {
+		e.preventDefault();
+		setError('');
+		try {
+			const response = await axios.post(
+				`${import.meta.env.VITE_APP_BASE_URL}/users/login`,
+				userData
+			);
+			const user = await response.data;
+			setCurrentUser(user);
+			navigate('/');
+		} catch (err) {
+			setError(err.response.data.message);
+		}
+	};
+
+	return (
+		<section className="login">
+			<div className="container">
+				<h2>Sign In</h2>
+				<form className="form login__form" onSubmit={loginUser}>
+					{error && <p className="form__error-message">{error}</p>}
+					<input
+						type="email"
+						placeholder="Email"
+						name="email"
+						value={userData.email}
+						onChange={changeInputHandler}
+					/>
+					<input
+						type="password"
+						placeholder="Password"
+						name="password"
+						value={userData.password}
+						onChange={changeInputHandler}
+					/>
+					<button type="submit" className="btn primary">
+						Login
+					</button>
+				</form>
+				<small>
+					Don't have an account yet? <Link to={'/register'}>Sign up</Link>
+				</small>
+			</div>
+		</section>
+	);
+};
+
+export default Login;
